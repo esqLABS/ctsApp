7e090ebffcb008fb22996fa5d6771673cdb84545
Felix MIL <dev@felixmil.com>
2025-01-29 11:38:07 +0100

    Make value boxes reactive depending on data

--- R/mod_results_ddi.R
+++ R/mod_results_ddi.R
@@ -10,22 +10,10 @@
 mod_mod_results_ddi_ui <- function(id) {
   ns <- NS(id)
 
-  vbs_data <- list(
-    auc = list(
-      title = "AUC ratio",
-      variable = "auc_r"
-    ),
-    cmax = list(
-      title = "Cmax ratio",
-      variable = "cmax_r"
-    )
-  )
-
   card(
     card_body(
-      mod_results_values_ui(ns("results_values_interactions"),
-                            vbs_data = vbs_data),
-      mod_results_plot_ui(ns("results_plot_interactions")))
+      mod_results_vbs_ui(ns("results_vbs_ddi")),
+      mod_results_plot_ui(ns("results_plot_ddi")))
   )
 }
 
@@ -36,20 +24,26 @@ mod_mod_results_ddi_server <- function(id, r){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
 
-    vbs_variables <- c("auc_r", "cmax_r")
-
-    vbs_values <- reactiveValues()
+    vbs_data <- reactiveValues()
 
     observe({
-      vbs_values$auc_r = runif(1)
-      vbs_values$cmax_r = runif(1)
+      vbs_data$auc = list(
+            title = "AUC ratio",
+            value = 0.53,
+            threshold = 0.5,
+            threshold_message = "Interaction detected",
+            threshold_theme = "danger"
+          )
+      vbs_data$cmax = list(
+            title = "Cmax ratio",
+            value = runif(1)
+          )
     })
 
-    mod_results_values_server("results_values_interactions",
-                              variables = vbs_variables,
-                              values = vbs_values)
 
-    mod_results_plot_server("results_plot_interactions")
+    mod_results_vbs_server("results_vbs_ddi", vbs_data)
+    mod_results_plot_server("results_plot_ddi")
+
   })
 }
 
--- R/mod_results_pk.R
+++ R/mod_results_pk.R
@@ -10,22 +10,21 @@
 mod_results_pk_ui <- function(id) {
   ns <- NS(id)
 
-  vbs_data <- list(
-    auc = list(
-      title = "AUC",
-      variable = "auc"
-    ),
-    cmax = list(
-      title = "Cmax",
-      variable = "cmax"
-    )
-  )
-
+  # vbs_data <- list(
+  #   auc = list(
+  #     title = "AUC",
+  #     variable = "auc"
+  #   ),
+  #   cmax = list(
+  #     title = "Cmax",
+  #     variable = "cmax"
+  #   )
+  # )
+  #
   card(
     card_body(
-      mod_results_values_ui(ns("results_values_general"),
-                            vbs_data = vbs_data),
-      mod_results_plot_ui(ns("results_plot_general")))
+      mod_results_vbs_ui(ns("results_vbs_pk")),
+      mod_results_plot_ui(ns("results_plot_pk")))
   )
 
 }
@@ -37,20 +36,22 @@ mod_results_pk_server <- function(id, r){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
 
-    vbs_variables <- c("auc", "cmax")
-
-    vbs_values <- reactiveValues()
+    vbs_data <- reactiveValues()
 
     observe({
-      vbs_values$auc = runif(1)
-      vbs_values$cmax = runif(1)
+      vbs_data$auc = list(
+        title = "AUC",
+        value = runif(1)
+      )
+      vbs_data$cmax = list(
+        title = "Cmax",
+        value = runif(1,min = 300, max = 600)
+      )
     })
 
-    mod_results_values_server("results_values_general",
-                              variables = vbs_variables,
-                              values = vbs_values)
 
-    mod_results_plot_server("results_plot_general")
+    mod_results_vbs_server("results_vbs_pk", vbs_data)
+    mod_results_plot_server("results_plot_pk")
   })
 }
 
